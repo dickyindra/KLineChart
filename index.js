bed0172b54a12139aae94bb4b505271350f460f0
dickyindra <dickyindraofficial@gmail.com>
2022-01-12 11:37:44 +0700

    fix: remove developmet mode

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 'use strict'
-// if (process.env.NODE_ENV === 'production') {
-//   module.exports = require('./dist/klinecharts.min.js')
-// } else {
-//   module.exports = require('./dist/klinecharts.js')
-// }
-
-
-module.exports = require('./src/index')
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./dist/klinecharts.min.js')
+} else {
+  module.exports = require('./dist/klinecharts.js')
+}
